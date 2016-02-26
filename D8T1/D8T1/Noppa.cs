1773a2ab5d4ea7ef44db25f4e658742963f95884
Matti Schroderus <mattischroderus37@gmail.com>
2016-02-26 17:51:46 +0200

    26.2.2016_2

---
+++ D8T1/D8T1/Noppa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8T1
+{
+    class Noppa
+    {
+        private static readonly Random staticRand = new Random();
+
+        public static int Nopanheitto()
+        {
+            return staticRand.Next(6) + 1;
+        }
+    }
+}
---
+++ D8T1/D8T1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Summa = 0;
+            double Keskiarvo;
+            int NoppaTulos;
+            int Kertaa=0;
+            int Kertaa1;
+            int laskuri=1;
+
+            Console.Write("\n\n  Montako kertaa heitetaan: ");
+            Kertaa = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Kertaa1 = Kertaa;
+
+            while (Kertaa1 > 0)
+            {
+                NoppaTulos = Noppa.Nopanheitto();
+                Console.WriteLine("  ({0}) Tulos {1} ", laskuri, NoppaTulos);
+                Summa += NoppaTulos;
+                Kertaa1--;
+                laskuri++;
+            }
+
+            Keskiarvo = Summa / Kertaa;
+
+            Console.WriteLine("\n\n  Keskiarvo: {0}",Keskiarvo);
+            Console.WriteLine("\n\n");
+
+        }
+    }
+}
